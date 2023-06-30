473176df09875df101d080b35ff9e52c1cbeb13a
MagnusHJensen <magnus.holm.jensen@lego.com>
2023-06-30 21:03:20 +0200

    Added a storage stack

---
+++ stacks/StorageStack.ts
@@ -0,0 +1,19 @@
+import { Construct } from "constructs";
+import { Bucket, StackContext, Table } from "sst/constructs";
+
+export function StorageStack({ stack, app }: StackContext) {
+  const bucket = new Bucket(stack, "Uploads");
+
+  const table = new Table(stack, "Notes", {
+    fields: {
+      userId: "string",
+      noteId: "string",
+    },
+    primaryIndex: {
+      partitionKey: "userId",
+      sortKey: "noteId",
+    },
+  });
+
+  return { table, bucket };
+}
